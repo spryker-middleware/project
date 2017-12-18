3d51dd674e3ee9d377ce880f7f78cabf70bc6173
Eugene Varava <ievgen.varava@spryker.com>
2017-12-18 15:13:46 +0100

    SM-19: Added comments to config.

--- config/Shared/config_default-middleware-development.php
+++ config/Shared/config_default-middleware-development.php
@@ -15,7 +15,12 @@ $config[PropelConstants::ZED_DB_HOST] = '127.0.0.1';
 $config[PropelConstants::ZED_DB_PORT] = 5432;
 $config[PropelConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
 
-$config[ProcessConstants::PRODUCT_IMPORT_FILE_PATH] = APPLICATION_ROOT_DIR . '/files/product.txt';
-$config[ProcessConstants::PRODUCT_IMPORT_OUTPUT_PATH] = APPLICATION_ROOT_DIR . '/files/output/product_import.output';
+// Input file for map generator process
 $config[ProcessConstants::MAP_SOURCE_FILE_PATH] = APPLICATION_ROOT_DIR . '/files/map.json';
+// Output file for map generator process (to be used in product import map)
 $config[ProcessConstants::GENERATED_MAP_FILE_PATH] = APPLICATION_ROOT_DIR . '/files/config/product_import.map';
+
+// Input file for product import process
+$config[ProcessConstants::PRODUCT_IMPORT_FILE_PATH] = APPLICATION_ROOT_DIR . '/files/product.txt';
+// Output file for product import process (might be used to check results)
+$config[ProcessConstants::PRODUCT_IMPORT_OUTPUT_PATH] = APPLICATION_ROOT_DIR . '/files/output/product_import.output';
