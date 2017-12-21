a172b054114ded277ac1ccfd3ff9a0f369a7fdda
Valerii Pravoslavnyi <valerio8787@gmail.com>
2017-12-21 10:30:01 +0200

    Updated Logger configuration for PRODUCT_IMPORT_PROCESS

--- src/Middleware/Zed/Process/Business/Log/Config/ProductImportLoggerConfig.php
+++ src/Middleware/Zed/Process/Business/Log/Config/ProductImportLoggerConfig.php
@@ -3,12 +3,42 @@
 namespace Middleware\Zed\Process\Business\Log\Config;
 
 use Middleware\Shared\Process\ProcessConstants;
+use Monolog\Formatter\LogstashFormatter;
+use Monolog\Handler\StreamHandler;
 use Spryker\Shared\Config\Config;
 use SprykerMiddleware\Zed\Process\Business\Log\Config\MiddlewareLoggerConfig;
 
 class ProductImportLoggerConfig extends MiddlewareLoggerConfig
 {
     const NAME = 'ProductImport';
+
+    /**
+     * @return \Monolog\Handler\HandlerInterface[]
+     */
+    public function getHandlers(): array
+    {
+        $handlers = parent::getHandlers();
+        if (!$this->loggerSettings->getIsQuiet()) {
+            $handlers[] = $this->createStreamHandler();
+        }
+
+        return $handlers;
+    }
+
+    /**
+     * @return \Monolog\Handler\StreamHandler
+     */
+    protected function createStreamHandler(): StreamHandler
+    {
+        $streamHandler = new StreamHandler(
+            $this->getLogFilePath(),
+            $this->loggerSettings->getVerboseLevel()
+        );
+        $formatter = new LogstashFormatter(static::NAME);
+        $streamHandler->setFormatter($formatter);
+
+        return $streamHandler;
+    }
 
     /**
      * @return string
