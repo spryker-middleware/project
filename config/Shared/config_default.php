d394bbb70ce8d5f3412d22c132e102d2d03f2bb2
Mykola <mykola.ostrizhnii@spryker.com>
2019-06-13 12:54:40 +0300

    Making project work

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -7,6 +7,9 @@ use Spryker\Shared\Kernel\ClassResolver\Cache\Provider\File;
 use Spryker\Shared\Kernel\KernelConstants;
 use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\Log\LogConstants;
+use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Shared\PropelOrm\PropelOrmConstants;
+use Spryker\Zed\Propel\PropelConfig;
 
 $CURRENT_STORE = Store::getInstance()->getStoreName();
 
@@ -43,3 +46,22 @@ $config[LogConstants::LOG_FILE_PATH_ZED] = sprintf(
 $config[KernelConstants::AUTO_LOADER_CACHE_FILE_NO_LOCK] = false;
 $config[KernelConstants::AUTO_LOADER_UNRESOLVABLE_CACHE_ENABLED] = false;
 $config[KernelConstants::AUTO_LOADER_UNRESOLVABLE_CACHE_PROVIDER] = File::class;
+
+// ---------- Propel
+$config[PropelConstants::ZED_DB_ENGINE_MYSQL] = PropelConfig::DB_ENGINE_MYSQL;
+$config[PropelConstants::ZED_DB_ENGINE_PGSQL] = PropelConfig::DB_ENGINE_PGSQL;
+$config[PropelConstants::ZED_DB_SUPPORTED_ENGINES] = [
+    PropelConfig::DB_ENGINE_MYSQL => 'MySql',
+    PropelConfig::DB_ENGINE_PGSQL => 'PostgreSql',
+];
+$config[PropelConstants::SCHEMA_FILE_PATH_PATTERN] = APPLICATION_VENDOR_DIR . '/*/*/src/*/Zed/*/Persistence/Propel/Schema/';
+$config[PropelConstants::USE_SUDO_TO_MANAGE_DATABASE] = true;
+$config[PropelConstants::PROPEL_DEBUG] = false;
+$config[PropelOrmConstants::PROPEL_SHOW_EXTENDED_EXCEPTION] = true;
+$config[PropelConstants::ZED_DB_DATABASE] = 'DE_development_zed';
+$config[PropelConstants::ZED_DB_USERNAME] = 'development';
+$config[PropelConstants::ZED_DB_PASSWORD] = 'mate20mg';
+$config[PropelConstants::ZED_DB_HOST] = '127.0.0.1';
+$config[PropelConstants::ZED_DB_PORT] = 5432;
+$config[PropelConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
+//$config[PropelQueryBuilderConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
